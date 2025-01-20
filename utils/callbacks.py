5ccffe63f0981508576a2d37bd5bda761302c3fe
FennexFox <technocracy90@gmail.com>
2025-01-20 14:39:45 +0900

    fix data_assorted

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -3,7 +3,7 @@ import tensorflow, keras
 def callback_savemodel(model_name):
     return keras.callbacks.ModelCheckpoint(
         model_name,
-        monitor='val_loss',
+        monitor='val_accuracy',
         save_best_only=True,
         save_weights_only=False,
         verbose=0
