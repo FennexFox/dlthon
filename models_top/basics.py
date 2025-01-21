96d0710b7719f71250e7e90f73d5071726da7f07
FennexFox <technocracy90@gmail.com>
2025-01-21 15:53:13 +0900

    implement midsize2 and midsize3

--- models_top/basics.py
+++ models_top/basics.py
@@ -36,3 +36,38 @@ def midsize1():
     model.name = "basic_midsize1"
     
     return model
+
+def midsize2():
+    model = keras.Sequential([
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(512, activation='relu'),
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(512, activation='relu'),
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(256, activation='relu')
+    ])
+    
+    model.name = "basic_midsize1"
+    
+    return model
+
+def midsize3():
+    inputs = keras.Input(shape=(7, 7, 1280))
+    
+    x = layers.GlobalAveragePooling2D()(inputs)
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    
+    residual = x
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.add([x, residual])
+
+    output = layers.Dense(256, activation='relu')(x)
+    
+    model = keras.Model(inputs=inputs, outputs=output, name="basic_midsize3")
+    return model
