e8fa52bd8ca997d46ee56820b1a96ef1a4aeb503
FennexFox <technocracy90@gmail.com>
2025-01-21 13:14:03 +0900

    fix load_unified_dataset function

--- utils/data.py
+++ utils/data.py
@@ -27,33 +27,25 @@ def load_split_datasets(base_dir, image_size = (224, 224), batch_size = 64):
     
     return train_dataset, val_dataset, test_dataset
 
-def load_unified_datasets(base_dir, image_size = (224, 224), batch_size = 64):
+def load_unified_datasets(base_dir, image_size = (224, 224), batch_size = 64, val_split = 0.4):
     if not pathlib.Path(base_dir).exists():
         raise ValueError(f"Directory {base_dir} does not exist")
         
-    train_dataset = keras.utils.image_dataset_from_directory(
+    dataset = keras.utils.image_dataset_from_directory(
         pathlib.Path(base_dir),
-        validation_split=0.2,
-        subset="training",
+        validation_split=val_split,
+        subset="both",
         seed=123,
         image_size=image_size,
         batch_size=batch_size,
         label_mode='categorical'
     )
-
-    val_test_dataset = keras.utils.image_dataset_from_directory(
-        pathlib.Path(base_dir),
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=image_size,
-        batch_size=batch_size,
-        label_mode='categorical'
-    )
+    
+    train_dataset = dataset[0]
 
     # Split validation dataset into validation and test sets
-    val_batches = len(val_test_dataset) // 2
-    val_dataset = val_test_dataset.take(val_batches)
-    test_dataset = val_test_dataset.skip(val_batches)
+    val_batches = len(dataset[1]) // 2
+    val_dataset = dataset[1].take(val_batches)
+    test_dataset = dataset[1].skip(val_batches)
     
     return train_dataset, val_dataset, test_dataset
