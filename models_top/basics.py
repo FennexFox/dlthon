3e21ead2a176988e0b5dccaf8c9f760dd4b01854
FennexFox <technocracy90@gmail.com>
2025-01-22 20:28:04 +0900

    add midsize2 and midsize3 models

--- models_top/basics.py
+++ models_top/basics.py
@@ -36,3 +36,46 @@ def midsize1():
     model.name = "basic_midsize1"
     
     return model
+
+def midsize2():
+    model = keras.Sequential([
+        layers.GlobalAveragePooling2D(),
+        
+        layers.Dense(512, activation='relu'),
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
+
+        layers.Dense(512, activation='relu'),
+        layers.Dense(512, activation='relu'),
+        layers.Dropout(0.5),
+
+        layers.Dense(256, activation='relu')
+    ])
+    
+    model.name = "basic_midsize1"
+    
+    return model
+
+def midsize3():
+    inputs = keras.Input(shape=(7, 7, 1280))
+    x = layers.GlobalAveragePooling2D()(inputs)
+    x = layers.Dense(1024, activation='relu')(x)
+
+    residual = x
+    x = layers.Dense(1024, activation='relu')(x)
+    x = layers.Dense(1024, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.add([x, residual])
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    
+    residual = x
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.add([x, residual])
+    output = layers.Dense(256, activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    
+    model = keras.Model(inputs=inputs, outputs=output, name="basic_midsize3")
+    return model
