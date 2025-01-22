56de1968714a496c2f87ab1288e921507429972b
FennexFox <technocracy90@gmail.com>
2025-01-22 15:11:33 +0900

    create blue_sorted dataset and remove unused ones

--- models_top/basics.py
+++ models_top/basics.py
@@ -59,13 +59,14 @@ def midsize2():
 def midsize3():
     inputs = keras.Input(shape=(7, 7, 1280))
     x = layers.GlobalAveragePooling2D()(inputs)
-    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dense(1024, activation='relu')(x)
 
     residual = x
-    x = layers.Dense(512, activation='relu')(x)
-    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dense(1024, activation='relu')(x)
+    x = layers.Dense(1024, activation='relu')(x)
     x = layers.Dropout(0.5)(x)
     x = layers.add([x, residual])
+    x = layers.Dense(512, activation='relu')(x)
     x = layers.BatchNormalization()(x)
     
     residual = x
@@ -73,9 +74,8 @@ def midsize3():
     x = layers.Dense(512, activation='relu')(x)
     x = layers.Dropout(0.5)(x)
     x = layers.add([x, residual])
-    x = layers.BatchNormalization()(x)
-
     output = layers.Dense(256, activation='relu')(x)
+    x = layers.BatchNormalization()(x)
     
     model = keras.Model(inputs=inputs, outputs=output, name="basic_midsize3")
     return model
